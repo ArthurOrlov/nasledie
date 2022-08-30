34146978c1502e56333aaecbc5bc6ebd896328e8
ArthurOrlov <81583856+ArthurOrlov@users.noreply.github.com>
2022-08-30 23:23:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('#menu').classList.toggle('is-active')
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const followCursor = () => {
+      const el = document.querySelector('.follow-cursor')
+      window.addEventListener('mousemove', e => {
+        const target = e.target 
+        if (!target) return
+
+        if (target.closest('.container__block')) { 
+            el.classList.add('follow-cursor_active') 
+          } else { 
+            el.classList.remove('follow-cursor_active')
+          }
+  
+        el.style.left = e.pageX + 'px' 
+        el.style.top = e.pageY + 'px' 
+      })
+    }
+  
+    followCursor()
+  
+  })
